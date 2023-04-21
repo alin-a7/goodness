6acbdaf8dcff46f915166b02aed51cef19b0446f
Alina <almaksimovic01@gmail.com>
2023-04-21 20:19:43 +0300

    feat: add nestjs

--- server/src/main.ts
+++ server/src/main.ts
@@ -1,8 +1,16 @@
 import { NestFactory } from '@nestjs/core';
+
 import { AppModule } from './app.module';
 
-async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
-}
-bootstrap();
+const start = async () => {
+  try {
+    const PORT = process.env.PORT || 5000;
+    const app = await NestFactory.create(AppModule);
+
+    await app.listen(PORT, () => console.log(`Server started on PORT ${PORT}`));
+  } catch (e) {
+    console.log(e);
+  }
+};
+
+start();
