b4fa3a0c36ea8b8a24e4e58ec433f1ffc9606c87
Alina <almaksimovic01@gmail.com>
2023-06-07 00:21:40 +0300

    fix: fix errors

--- server/dist/auth/auth.controller.d.ts
+++
@@ -1,8 +0,0 @@
-import { CreateUserDto, LoginUserDto } from '../user/dto/user.dto';
-import { AuthService } from './auth.service';
-export declare class AuthController {
-    private authService;
-    constructor(authService: AuthService);
-    login(userDto: LoginUserDto): Promise<import("../user/user.schema").User>;
-    registration(userDto: CreateUserDto): Promise<import("../user/user.schema").User>;
-}
--- server/dist/deed/dto/comment.dto.d.ts
+++
@@ -1,6 +0,0 @@
-import { ObjectId } from 'mongoose';
-export declare class CreateCommentDto {
-    readonly author: string;
-    readonly text: string;
-    readonly deedId: ObjectId;
-}
